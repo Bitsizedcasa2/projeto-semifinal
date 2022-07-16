6a681adeabeb1e9eb2e6032f1415bf6006bfb2ba
Thiagolp7 <thiagolp.dev@gmail.com>
2022-07-16 15:01:17 -0300

    feat: criado estilização do component point count

--- react/PointsCount.tsx
+++ react/PointsCount.tsx
@@ -1,20 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import { useCssHandles } from 'vtex.css-handles'
 import s from './styles.module.css'
+import { IoWalletOutline } from 'react-icons/io5'
 
 // const CSS_HANDLES = [
 //   "pointsCountContainer",
+//   "button",
 //   "pointsCountTitle",
 //   "pointsCountValue"
 // ]
 
 export default function PointsCount(){
+  const [ menuIsActive, setMenuIsActive ] = useState(false);
 
   // const s = useCssHandles(CSS_HANDLES)
 
   return (
     <div className={s.pointsCountContainer}>
-      <p className={s.pointsCountTitle}>Pontos: <span className={s.pointsCountValue}>20000</span></p>
+      <button
+        className={s.pointsCountBtn}
+        onClick={() => setMenuIsActive(!menuIsActive)}
+        onBlur={() => setMenuIsActive(!menuIsActive)}
+      >
+        <IoWalletOutline/>
+      </button>
+
+      <div className={`${s.pointsCountMenu}
+      ${menuIsActive ? s.isActive : ""}`}>
+        <p className={s.pointsCountTitle}>Disponível:
+        </p>
+        <p className={s.pointsCountValue}>2000 pts</p>
+
+      </div>
+
     </div>
   )
 }
